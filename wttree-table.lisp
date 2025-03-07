57d205c38d312d701bff73099f5e9f8e9e7bc73e
Joe Marshall <joseph.marshall@nutanix.com>
2025-03-07 01:48:12 -0800

    Tidy.

--- wttree-table.lisp
+++ wttree-table.lisp
@@ -29,7 +29,8 @@
   (make-instance 'wttree-table :metadata (copy-list (metadata table)) :representation nil :test (test table)))
 
 (defmethod table/clear! ((table wttree-table))
-  (setf (representation table) nil))
+  (setf (representation table) nil)
+  table)
 
 (defmethod table/copy ((table wttree-table))
   (make-instance 'wttree-table :metadata (copy-list (metadata table))
@@ -41,14 +42,16 @@
         (fold-left (lambda (node key)
                      (node/remove (test->node-test (test table)) node key))
                    (representation table)
-                   keys)))
+                   keys))
+  table)
 
 (defmethod table/delete-keys ((table wttree-table) key-list)
   (setf (representation table)
         (fold-left (lambda (node key)
                      (node/remove (test->node-test (test table)) node key))
                    (representation table)
-                   key-list)))
+                   key-list))
+  table)
 
 (defmethod table/insert ((table wttree-table) key value)
   (make-instance 'wttree-table
@@ -76,26 +79,30 @@
     (values (node/k node) (node/v node))))
 
 (defmethod table/pop-maximum ((table wttree-table))
-  (multiple-value-bind (key value) (table/maximum table)
-    (values key value (make-instance 'wttree-table
-                                     :representation (node/remove-maximum (representation table))
-                                     :test (test table)))))
+  (let ((node (node/maximum (representation table))))
+    (values (node/k node)
+            (node/v node)
+            (make-instance 'wttree-table
+                           :representation (node/remove-maximum (representation table))
+                           :test (test table)))))
 
 (defmethod table/pop-maximum! ((table wttree-table))
-  (multiple-value-bind (key value) (table/maximum table)
+  (let ((node (node/maximum (representation table))))
     (setf (representation table) (node/remove-maximum (representation table)))
-    (values key value)))
+    (values (node/k node) (node/v node) table)))
 
 (defmethod table/pop-minimum ((table wttree-table))
-  (multiple-value-bind (key value) (table/minimum table)
-    (values key value (make-instance 'wttree-table
-                                     :representation (node/remove-minimum (representation table))
-                                     :test (test table)))))
+  (let ((node (node/minimum (representation table))))
+    (values (node/k node)
+            (node/v node)
+            (make-instance 'wttree-table
+                           :representation (node/remove-maximum (representation table))
+                           :test (test table)))))
 
 (defmethod table/pop-minimum! ((table wttree-table))
-  (multiple-value-bind (key value) (table/minimum table)
-    (setf (representation table) (node/remove-minimum (representation table)))
-    (values key value)))
+  (let ((node (node/minimum (representation table))))
+    (setf (representation table) (node/remove-maximum (representation table)))
+    (values (node/k node) (node/v node) table))
 
 (defmethod table/remove ((table wttree-table) &rest keys)
   (make-instance 'wttree-table
@@ -139,7 +146,8 @@
                  :test (test left)))
 
 (defmethod table/union! ((left wttree-table) (right wttree-table))
-  (setf (representation left) (node/union (test->node-test (test left)) (representation left) (representation right))))
+  (setf (representation left) (node/union (test->node-test (test left)) (representation left) (representation right)))
+  left)
 
 (defmethod table/union-merge ((left wttree-table) (right wttree-table) merge)
   (make-instance 'wttree-table
@@ -147,7 +155,8 @@
                  :test (test left)))
 
 (defmethod table/union-merge! ((left wttree-table) (right wttree-table) merge)
-  (setf (representation left) (node/union-merge (test->node-test (test left)) (representation left) (representation right) merge)))
+  (setf (representation left) (node/union-merge (test->node-test (test left)) (representation left) (representation right) merge))
+  left)
 
 (defmethod table/values ((table wttree-table))
   (node/values (representation table)))
